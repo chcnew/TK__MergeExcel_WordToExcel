0885eaebce116c9f13ece65b1e02bda34e32430f
chcnew <857339512@qq.com>
2022-01-04 18:04:27 +0800

    update

--- merge_excel.py
+++ merge_excel.py
@@ -11,38 +11,25 @@ def merge_excel():
     # 结合Tk设计
     window = tk.Tk()
     window.withdraw()
-    print("请选择工单号命名的文件夹所在的文件夹！")
-    print("存放形式：*/工单号/处理方案/*.excel")
+    print("请选择需要合并的Excel文件所在的文件夹！")
     path = filedialog.askdirectory()
-    # path = os.getcwd()
+    print("正在处理，请稍后...")
     names = os.listdir(path)
     lst = []
-    lst1 = []
-    lst2 = []
     for i in names:
-        if 'WF364363' in i:
-            names1 = os.listdir(path + '/' + i + '/处理方案/')
-            lstname1 = [(path + '/' + i + '/处理方案/' + p) for p in names1]
-            # print(lstname1)
-            for j in lstname1:
-                if 'xlsx' == j.split(".")[1]:
-                    lst1.append(j)
-                    lst2.append(i)
-    # print(lst1)
-    # print(lst2)
+        if i.split(".")[1] == "xlsx" or i.split(".")[1] == "xls":
+            lst.append(path + '/' + i)
 
-    df = pd.read_excel(lst1[0], sheet_name=0)
-    df['工单号'] = lst2[0]
+    df = pd.read_excel(lst[0], sheet_name=0)
     empty_df = pd.DataFrame(columns=df.columns)
 
     n = 0
-    for x, y in zip(lst1, lst2):
+    for x in lst:
         df = pd.read_excel(x, sheet_name=0)
-        df['工单号'] = lst2[n]
         empty_df = empty_df.append(df, ignore_index=True)
         n = n + 1
-        print("处理完成：" + str(y))
         time.sleep(0.0005)
 
     empty_df.to_excel('./' + 'MergeExcel文件' + datetime.datetime.now().strftime('%Y%m%d-%H%M%S') + '.xlsx', index=False)
+    print("处理完成。")
     sys.exit(0)
--- wordtoexcel.py
+++ wordtoexcel.py
@@ -13,26 +13,21 @@ def wordtoexcel():
     # 结合Tk设计
     window = tk.Tk()
     window.withdraw()
-    print("请选择工单号命名的文件夹所在的文件夹！")
-    print("存放形式：*/工单号/处理方案/*.docx")
+    print("请选择需要拆分的Word文件所在的文件夹！")
     path = filedialog.askdirectory()
-    # path = os.getcwd()
     names = os.listdir(path)
     lst1 = []
     for i in names:
-        if 'WF364363' in i:
-            names1 = os.listdir(path + '/' + i + '/处理方案')
-            for j in names1:
-                if ".docx" in j:
-                    path1 = path + '/' + i + '/处理方案/' + j
-                    document = Document(path1)
-                    # 获取所有段落
-                    all_paragraphs = document.paragraphs
-                    lst = [j.text for j in all_paragraphs]
-                    lst.insert(0, i)
-                    lst1.append(lst)
-                    print('处理完毕：' + i)
-                    time.sleep(0.0005)
+        if i.split(".")[1] == "docx" or i.split(".")[1] == "doc":
+            path1 = path + '/' + i
+            document = Document(path1)
+            # 获取所有段落
+            all_paragraphs = document.paragraphs
+            lst = [j.text for j in all_paragraphs]
+            lst.insert(0, i)
+            lst1.append(lst)
+            print('处理完毕：' + i)
+            time.sleep(0.0005)
 
     # 二维列表转Dataframe
     df = frame.DataFrame(lst1)
